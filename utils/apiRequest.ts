ec065894ef33862a1ee97ae31e13afb3eeff30c9
Aslan Evloev <eevloeev@gmail.com>
2023-01-03 23:53:23 +0530

    fix: API baseURL fixed

--- utils/apiRequest.ts
+++ utils/apiRequest.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios"
 
 const apiRequest = (config: AxiosRequestConfig) =>
   axios({
-    baseURL: `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/v1/`,
+    baseURL: `${process.env.API_URL}/api/v1/`,
     ...config,
   })
 
