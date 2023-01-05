114ca39ef04094bd207011187d0c3a77b97820d6
Aslan Evloev <eevloeev@gmail.com>
2023-01-06 02:01:09 +0530

    fix: HTTP headers changed

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/advend.js",
+        source: "/api/*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
